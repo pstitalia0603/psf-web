37c7e950331a692cac3d47e1739e7fbbe78469c8
pstitalia0603 <paula@fragomeni.us>
2024-07-31 20:23:03 -0400

    node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian'
+import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			plugins: [
-				// Generate the Obsidian vault pages.
-				starlightObsidian({
-				  vault: 'TEST_PUBLISH',
-				}),
-			  ],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				   // Add the generated sidebar group to the sidebar.
-				   obsidianSidebarGroup,
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    plugins: [
+    // Generate the Obsidian vault pages.
+    starlightObsidian({
+      vault: 'TEST_PUBLISH'
+    })],
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        slug: 'guides/example'
+      }]
+    },
+    // Add the generated sidebar group to the sidebar.
+    obsidianSidebarGroup, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
