8e14331cc1615af0b1d241556f64d4d8c3647559
pstitalia0603 <paula@fragomeni.us>
2024-07-31 20:43:46 -0400

    build dist

---
+++ dist/server/manifest_CTUoA2Kn.mjs
@@ -0,0 +1,6 @@
+import '@astrojs/internal-helpers/path';
+import 'cookie';
+export { m as manifest } from './chunks/_@astrojs-manifest_BDhzDCLN.mjs';
+import 'kleur/colors';
+import './chunks/astro/server_B3XybpSG.mjs';
+import 'clsx';
